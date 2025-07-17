76862621b593480d5a54643f7b0e7694bfe468fc
elias pinheiro pereira <eliaspinheiropereiraa@gmail.com>
2025-07-17 17:34:49 -0300

    criando a service, criando injeção de dependencia e fazendo pequenos ajustes

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+-- CREATE TABLE tb_user(
+--     id int,
+--     nome varchar(255),
+--     email varchar(255),
+--     senha varchar(255),
+--     telefone varchar(255)
+-- );
+
+INSERT INTO tb_user (id, nome, email, senha, telefone) VALUES (1,'teste1','teste1@gmail.com', '12345','12345-54-321');
+INSERT INTO tb_user (id, nome, email, senha, telefone) VALUES (2,'teste2','teste2@gmail.com', '12345','12345-54-321');
